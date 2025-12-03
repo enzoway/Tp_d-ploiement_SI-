01ea3e50bcf6f7a963f3369f8ebe35d77a21b964
enzoway <enzomarie488@gmail.com>
2025-12-03 11:28:31 +0100

    Feat: Ajout de la configuration Docker (Compose, Dockerfile, SQL) et adaptation du code pour le déploiement réseau

--- database/init.sql
+++ database/init.sql
@@ -5,7 +5,6 @@ USE tp_deploiement_si;
 -- --------------------------------------------------------
 
 -- 1. Table : utilisateurs
--- Basé sur ton image (Admin/enzo, Opérateur/lucas)
 CREATE TABLE IF NOT EXISTS utilisateurs (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nom VARCHAR(255) NOT NULL,
@@ -21,7 +20,6 @@ INSERT INTO utilisateurs (id, nom, email, mot_de_passe) VALUES
 -- --------------------------------------------------------
 
 -- 2. Table : automates
--- Basé sur ton image (Automate 1, 2 et 5)
 CREATE TABLE IF NOT EXISTS automates (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nom VARCHAR(255) NOT NULL,
@@ -37,7 +35,7 @@ INSERT INTO automates (id, nom, adresse_ip) VALUES
 -- --------------------------------------------------------
 
 -- 3. Table : variables
--- Inclut les colonnes min, max, unite ajoutées récemment
+-- MISE À JOUR : Ajout des colonnes type, min, max
 CREATE TABLE IF NOT EXISTS variables (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nom VARCHAR(255) NOT NULL,
@@ -45,23 +43,25 @@ CREATE TABLE IF NOT EXISTS variables (
   registre VARCHAR(50) NOT NULL,
   frequence INT DEFAULT 5,
   unite VARCHAR(50),
+  
+  -- NOUVELLES COLONNES
+  type VARCHAR(20) DEFAULT 'coil', -- 'coil' (bit) ou 'holding' (mot)
   min FLOAT DEFAULT NULL,
   max FLOAT DEFAULT NULL,
+  
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (automate_id) REFERENCES automates(id) ON DELETE CASCADE
 );
 
-INSERT INTO variables (id, nom, automate_id, registre, frequence, unite, min, max) VALUES
-(1, 'Température', 1, '505', 5, '°C', 0, 1),
-(2, 'bp-dpcy', 2, '503', 3, 'P', 0, 1),
-(3, 'bp-arcy', 2, '504', 3, 'kk', 0, 1),
-(11, 'bp_tl5', 5, '505', 1, 'scal', 0, 1);
+-- Exemples d'insertion avec les nouveaux champs
+INSERT INTO variables (id, nom, automate_id, registre, frequence, unite, type, min, max) VALUES
+(1, 'Température', 1, '505', 5, '°C', 'coil', 0, 1), -- Exemple bit (ton ancien exemple)
+(2, 'bp-dpcy', 2, '503', 3, 'P', 'coil', 0, 1),
+(13, 'tempzone4', 1, '400', 3, '°C', 'holding', 0, 100); -- Exemple mot (température)
 
 -- --------------------------------------------------------
 
 -- 4. Table : mesures
--- Stocke l'historique. On ne met pas de données par défaut ici, 
--- le script Node.js (Poller) va remplir ça très vite.
 CREATE TABLE IF NOT EXISTS mesures (
   id INT AUTO_INCREMENT PRIMARY KEY,
   variable_id INT NOT NULL,
@@ -70,5 +70,4 @@ CREATE TABLE IF NOT EXISTS mesures (
   FOREIGN KEY (variable_id) REFERENCES variables(id) ON DELETE CASCADE
 );
 
--- Index pour accélérer l'affichage des graphiques (Important pour les requêtes ORDER BY horodatage)
 CREATE INDEX idx_mesures_variable_horodatage ON mesures(variable_id, horodatage);
